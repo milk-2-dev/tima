030ec6fe4ba54f8c93b7a5df4b1f0504ed68b06f
milk <milk00000000000000@gmail.com>
2025-10-05 21:42:03 +0200

    create date filter

---
+++ src/components/filters/dateFilter.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Calendar } from "@/components/ui/calendar";
+import { Button } from "@/components/ui/button";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+import { ChevronDownIcon } from "lucide-react";
+
+function DateFilter() {
+  const [open, setOpen] = React.useState(false);
+  const [date, setDate] = React.useState<Date | undefined>(undefined);
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          id="date"
+          className="w-48 justify-between font-normal"
+        >
+          {date ? date.toLocaleDateString() : "Select date"}
+          <ChevronDownIcon />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto overflow-hidden p-0" align="start">
+        <Calendar
+          mode="single"
+          selected={date}
+          captionLayout="dropdown"
+          onSelect={(date) => {
+            setDate(date);
+            setOpen(false);
+          }}
+        />
+      </PopoverContent>
+    </Popover>
+  );
+}
+
+export default DateFilter;
